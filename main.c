87f21d794f548677577fe9e56dee5801b7e70f1a
Mathias Helsen <helsen.mathias@gmail.com>
2017-10-04 18:13:21 +0200

    das light are blinken!

--- main.c
+++ main.c
@@ -1,11 +1,27 @@
 #include "OS_Core/tinyOS.h"
 
+#include "stm32f4xx_gpio.h"
+
 int main(void)
 {
 
+	uint16_t rr = 0x8000;
+	uint32_t t;
 	setupIO( );
 	for(;;)
 	{
+		for(t = 0; t < 1680000; t++)
+		{
+
+		}
+
+		rr = rr >> 1;
+		if( rr < 0x1000 )
+		{
+			rr = 0x8000;
+		}
+
+		GPIO_Write(GPIOD, rr);
 
 	}
 	return 0;
